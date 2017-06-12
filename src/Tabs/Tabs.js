895c429e1d2d9949cf03dfe7f9b5f2cf6bd2f82d
dimik <dimik@yandex.ru>
2017-06-12 13:49:43 +0700

    fix(components): TabBar improvements

--- src/Tabs/Tabs.js
+++ src/Tabs/Tabs.js
@@ -8,28 +8,71 @@ class Tabs extends MDCComponent {
   static displayName = 'Tabs'
 
   static propTypes = {
+    accent: PropTypes.bool,
+    activeTab: PropTypes.number,
     children: PropTypes.node,
     className: PropTypes.string,
     iconTabs: PropTypes.bool,
     iconWithTextTabs: PropTypes.bool,
+    onChange: PropTypes.func,
+    primary: PropTypes.bool,
+  }
+
+  static defaultProps = {
+    onChange: () => {},
+  }
+
+  componentDidMount() {
+    super.componentDidMount()
+
+    this._setupListeners()
+  }
+
+  componentWillUnmount() {
+    this._clearListeners()
+
+    super.componentWillUnmount()
   }
 
   attachTo(el) {
     return new MDCTabs(el)
   }
 
+  _setupListeners() {
+    this.listen(
+      'MDCTabBar:change',
+      this.changeListener_ = e => this.props.onChange(
+        e,
+        e.detail,
+      )
+    )
+  }
+
+  _clearListeners() {
+    this.unlisten(
+      'MDCTabBar:change',
+      this.changeListener_
+    )
+  }
+
   render() {
     const {
+      accent,
+      activeTab,
       children,
       className,
       iconTabs,
       iconWithTextTabs,
+      onChange,
+      primary,
       ...otherProps,
     } = this.props
     const cssClasses = classNames({
       'mdc-tab-bar': true,
       'mdc-tab-bar--icon-tabs': iconTabs,
       'mdc-tab-bar--icons-with-text': iconWithTextTabs,
+      'mdc-tab-bar--indicator-accent': accent,
+      'mdc-tab-bar--indicator-primary': primary,
     }, className)
     return (
       <nav
@@ -37,7 +80,14 @@ class Tabs extends MDCComponent {
         className={cssClasses}
         ref={el => this.root_ = el}
       >
-        {children}
+        {activeTab >= 0 ?
+          React.Children.map(children, (child, index) =>
+            React.cloneElement(
+              child,
+              {active: index === activeTab}
+            )
+          ) : children
+        }
         <span className="mdc-tab-bar__indicator" />
       </nav>
     )
--- src/Tabs/index.js
+++ src/Tabs/index.js
@@ -1,4 +1,5 @@
 export Tabs from './Tabs'
+export {default as TabBar} from './Tabs'
 export Tab from './Tab'
 
 export default from './Tabs'
